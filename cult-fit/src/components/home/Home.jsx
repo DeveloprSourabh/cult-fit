e3972401696c334385b9ee6c09bcf9bd5dc24513
Sourabh <developr.sourabh@gmail.com>
2022-08-26 08:50:02 +0530

    create new react-app and add home page

---
+++ cult-fit/src/components/home/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <a href="/">cult.fit</a>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h5>FITNESS</h5>
+        </div>
+        <div>
+          <h5>CARE</h5>
+        </div>
+        <div>
+          <h5>MIND</h5>
+        </div>
+        <div>
+          <h5>STORE</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
