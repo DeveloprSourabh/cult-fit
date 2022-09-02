a2ad77dad40d1bcc977ed85b82cfdcdb61eb7b13
Sourabh <developr.sourabh@gmail.com>
2022-09-02 11:58:06 +0530

    add data in api

--- cult-fit/src/components/store/Cardapi.jsx
+++ cult-fit/src/components/store/Cardapi.jsx
@@ -33,13 +33,77 @@ const Cardapi = [
     preprize: "₹ 3299",
     off: "50% off",
   },
+
+  // second row
+  {
+    heading: "JUST LAUNCHED",
+    subheading: "NEW ARRIVALS",
+    imgsrc:
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/2dioFsRcvQ8uu43WTe8zRqzc",
+    about: "Ombre Yoga Tights",
+    prize: "₹ 2299",
+    preprize: "₹ 3699",
+    off: "40% off",
+  },
   {
     imgsrc:
-      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/y9Ly6y2KSidVkL51S1ZeYv9Z",
-    about: "LAYR Solid Sweatshirt",
-    prize: "₹ 1649",
-    preprize: "₹ 3299",
-    off: "50% off",
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/12Uc17w8J6Mx6xPrySbZjkXh",
+    about: "2 in 1 Lightweight Yoga Mat",
+    prize: "₹ 623",
+    preprize: "₹ 1199",
+    off: "48% off",
+  },
+  {
+    imgsrc:
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/8LKu6LCyX4LqdK3Rpc1Hkjw7",
+    about: "2 in 1 Lightweight Yoga Mat",
+    prize: "₹ 623",
+    preprize: "₹ 1199",
+    off: "48% off",
+  },
+  {
+    imgsrc:
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/UFEqV6v4mHfdL8ndaTwu5AQE",
+    about: "2 in 1 Lightweight Yoga Mat",
+    prize: "₹ 623",
+    preprize: "₹ 1199",
+    off: "48% off",
+  },
+
+  // THIRD ROW
+
+  {
+    heading: "JUST LAUNCHED",
+    imgsrc:
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/GpeKVjiXj2nnH9eafrutHxfm",
+    about: "Smartrun c1: Bluetooth enabled treadmill with 6-level incline",
+    prize: "₹ 35499",
+    preprize: "₹ 68999",
+    off: "48% off",
+  },
+  {
+    imgsrc:
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/GpeKVjiXj2nnH9eafrutHxfm",
+    about: "Smartrun c1: Bluetooth enabled treadmill with 6-level incline",
+    prize: "₹ 35499",
+    preprize: "₹ 68999",
+    off: "48% off",
+  },
+  {
+    imgsrc:
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/GpeKVjiXj2nnH9eafrutHxfm",
+    about: "Smartrun c1: Bluetooth enabled treadmill with 6-level incline",
+    prize: "₹ 35499",
+    preprize: "₹ 68999",
+    off: "48% off",
+  },
+  {
+    imgsrc:
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/GpeKVjiXj2nnH9eafrutHxfm",
+    about: "Smartrun c1: Bluetooth enabled treadmill with 6-level incline",
+    prize: "₹ 35499",
+    preprize: "₹ 68999",
+    off: "48% off",
   },
 ];
 
