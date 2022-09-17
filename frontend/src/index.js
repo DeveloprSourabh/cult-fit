6a47ac9c60c3e4e148b679e419f88e8d8c0df42c
Sourabh <developr.sourabh@gmail.com>
2022-09-17 16:23:55 +0530

    delete file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
-<<<<<<< HEAD:frontend/src/index.js
-=======
 import App from "./App";
->>>>>>> review:cult-fit/src/index.js
 import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
